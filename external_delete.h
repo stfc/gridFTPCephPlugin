c9fc39ce3a8355a67e36da93f19aeaf63e8e6682
Ian Johnson <ijjorama@gmail.com>
2017-03-21 16:34:57 +0000

    Add external_delete.h wrapper to callout to an external delete program

--- external_delete.h
+++ external_delete.h
@@ -5,27 +5,24 @@
  */
 
 /* 
- * File:   gridftp_checkaccess.h
+ * File:   external_delete.h
  * Author: ijj
  *
  * Created on August 2, 2016, 1:14 PM
  */
 
-#ifndef GRIDFTP_CHECKACCESS_H
-#define GRIDFTP_CHECKACCESS_H
+#ifndef EXTERNAL_DELETE_H
+#define EXTERNAL_DELETE_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int checkAccess(const char* authdbprog, const char* authdbfile, 
-        const char* user, const char* operation, const char* path);
-  
-
+   int external_delete(const char* deleteprog, const char* conf, const char* pathname/* , const char* chunksize */);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* GRIDFTP_CHECKACCESS_H */
+#endif /* EXTERNAL_DELETE_H */
 
